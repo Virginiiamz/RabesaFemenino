0f9501ef72211f5e6d06a5fbedac6c86739e1f3d
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2025-03-15 14:35:45 +0100

    Cuando crea un entrenador, tambien crea un usuario

--- backend_rabesa/controllers/entrenadorController.js
+++ backend_rabesa/controllers/entrenadorController.js
@@ -6,16 +6,65 @@ const initModels = require("../models/init-models.js").initModels;
 // Crear la instancia de sequelize con la conexión a la base de datos
 const sequelize = require("../config/sequelize.js");
 
+// Para comparar contraseñas cifradas
+const bcrypt = require("bcrypt");
+// Librería de manejo de JWT
+const jwt = require("jsonwebtoken");
+
 // Cargar las definiciones del modelo en sequelize
 const models = initModels(sequelize);
 // Recuperar el modelo plato
 const Entrenador = models.entrenadores;
+const Usuario = models.usuario;
 
 class EntrenadorController {
   async createEntrenador(req, res) {
-    const entrenador = req.body;
+    const { correo, contrasena, nombre, edad, rol, fecha_ingreso, idclub } =
+      req.body;
+    // const entrenador = req.body;
 
     try {
+      const existingUser = await Usuario.findOne({ where: { correo } });
+      if (existingUser) {
+        return res
+          .status(400)
+          .json(
+            Respuesta.error(
+              null,
+              "Ya existe un usuario con ese correo electrónico."
+            )
+          );
+      }
+
+      // Cifrar la contraseña
+      const hashedPassword = await bcrypt.hash(contrasena, 10); // 10 es el nivel de "salting" (puedes ajustarlo)
+
+      // Crear el nuevo usuario
+      const newUser = await Usuario.create({
+        correo,
+        contrasena: hashedPassword, // Guardamos la contraseña cifrada
+        rol: "Entrenador",
+      });
+
+      // Responder con éxito
+      delete newUser.dataValues.contrasena; // Eliminar la contraseña del objeto de respuesta
+
+      const idusuario = newUser.dataValues.idusuario;
+
+      console.log("idUsuario: " + idusuario);
+      
+      const entrenador = {
+        nombre,
+        edad,
+        rol,
+        fecha_ingreso,
+        idclub,
+        idusuario,
+      };
+
+      console.log("entrenador", entrenador);
+      
+
       const nuevoEntrenador = await Entrenador.create(entrenador);
 
       res
@@ -25,12 +74,7 @@ class EntrenadorController {
       logMensaje("Error :" + err);
       res
         .status(500)
-        .json(
-          Respuesta.error(
-            null,
-            `Error al crear un entrenador nuevo: ${entrenador}`
-          )
-        );
+        .json(Respuesta.error(null, `Error al crear un entrenador nuevo`));
     }
   }
 }
