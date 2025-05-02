b88b1d2ed4d2a78fd00cc687c317cadc183296ed
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2025-05-02 13:23:24 +0200

    Dado estilo al Login

--- frontend_rabesa/src/components/Login.jsx
+++ frontend_rabesa/src/components/Login.jsx
@@ -1,8 +1,18 @@
-import { Box, Button, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormHelperText,
+  InputLabel,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { apiUrl } from "../config";
 import { useNavigate } from "react-router";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import useUserStore from "../stores/useUserStore";
+import logoRabesa from "../assets/img/logo_rabesa.jpg";
+import { playNotificationSound } from "../utils/Funciones";
+import { enqueueSnackbar } from "notistack";
 
 function Login() {
   const navigate = useNavigate();
@@ -10,11 +20,53 @@ function Login() {
     correo: "",
     contrasena: "",
   });
-  const [errors, setErrors] = useState({});
   const { setUser } = useUserStore();
+  const [validacion, setValidacion] = useState({
+    correo: false,
+    contraseña: false,
+  });
+  const notificacionError = useRef(null);
+  const notificacion = useRef(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (formData.correo === "" && formData.contrasena === "") {
+      setValidacion({ correo: true, contraseña: true });
+      return;
+    }
+
+    if (formData.correo === "") {
+      setValidacion({ ...validacion, correo: true });
+      return;
+    }
+
+    if (!formData.correo.includes("@gmail.com")) {
+      playNotificationSound(notificacionError);
+
+      enqueueSnackbar("El correo debe contener (@gmail.com)", {
+        variant: "error",
+        autoHideDuration: 3000,
+        anchorOrigin: { vertical: "bottom", horizontal: "right" },
+      });
+      return;
+    }
+
+    if (formData.contrasena === "") {
+      setValidacion({ ...validacion, contraseña: true });
+      return;
+    }
+
+    // if (formData.contrasena.length < 8) {
+    //   playNotificationSound(notificacionError);
+
+    //   enqueueSnackbar("La contraseña debe tener al menos 8 caracteres", {
+    //     variant: "error",
+    //     autoHideDuration: 3000,
+    //     anchorOrigin: { vertical: "bottom", horizontal: "right" },
+    //   });
+    //   return;
+    // }
 
     try {
       const response = await fetch(apiUrl + "/usuario/login", {
@@ -29,17 +81,36 @@ function Login() {
       const data = await response.json();
 
       if (response.ok) {
-        
-        alert(data.mensaje);
+        playNotificationSound(notificacion);
+
+        enqueueSnackbar(data.mensaje, {
+          variant: "success",
+          autoHideDuration: 3000,
+          anchorOrigin: { vertical: "bottom", horizontal: "right" },
+        });
+
         setUser(data.datos);
-        navigate("/home/dashboard"); // Redirige tras el login exitoso
+
+        // Redirige tras el login exitoso
+        setTimeout(() => {
+          navigate("/home/dashboard");
+        }, 1000);
       } else {
-        alert(data.mensaje);
-        // setErrors({ apiError: data.mensaje || "Credenciales incorrectas." });
+        playNotificationSound(notificacionError);
+
+        enqueueSnackbar(data.mensaje, {
+          variant: "error",
+          autoHideDuration: 3000,
+          anchorOrigin: { vertical: "bottom", horizontal: "right" },
+        });
       }
     } catch (error) {
-      setErrors({
-        apiError: "Error de red. Inténtalo de nuevo más tarde." + error,
+      playNotificationSound(notificacionError);
+
+      enqueueSnackbar("Error de red. Inténtalo de nuevo más tarde.", {
+        variant: "error",
+        autoHideDuration: 3000,
+        anchorOrigin: { vertical: "bottom", horizontal: "right" },
       });
     }
   };
@@ -51,32 +122,115 @@ function Login() {
 
   return (
     <>
-      <h1>Login</h1>
+      <audio
+        ref={notificacionError}
+        src="/sonido/notificacion_error.mp3"
+        preload="auto"
+      />
+      <audio ref={notificacion} src="/sonido/notificacion.mp3" preload="auto" />
+
       <Box
-        component="form"
-        sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
-        noValidate
-        autoComplete="off"
-        onSubmit={handleSubmit}
+        sx={{
+          border: "1px solid red",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#F1F8FF",
+        }}
       >
-        <TextField
-          id="outlined-basic"
-          label="Correo"
-          variant="outlined"
-          name="correo"
-          value={formData.correo}
-          onChange={handleChange}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Contraseña"
-          variant="outlined"
-          type="password"
-          name="contrasena"
-          value={formData.contrasena}
-          onChange={handleChange}
-        />
-        <Button variant="outlined" type="submit">Iniciar sesion</Button>
+        <Box
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            border: "1px solid #BDBDBD",
+            backgroundColor: "white",
+            borderRadius: "10px",
+            padding: "20px",
+            gap: "1rem",
+          }}
+        >
+          <img
+            src={logoRabesa}
+            style={{ width: "4rem", alignSelf: "center" }}
+          ></img>
+          <Typography
+            sx={{
+              fontFamily: "'Open sans'",
+              fontSize: "22px",
+              color: "black",
+              padding: "0px",
+              margin: "0px",
+              textAlign: "center",
+            }}
+          >
+            Iniciar sesión en Rabesa Femenino
+          </Typography>
+          <Box
+            component="form"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "0.5rem",
+              width: "50ch",
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={handleSubmit}
+          >
+            <p style={{ margin: 0, color: "black" }}>Correo electrónico:</p>
+            <TextField
+              hiddenLabel
+              id="outlined-basic"
+              variant="outlined"
+              name="correo"
+              color=""
+              value={formData.correo}
+              onChange={handleChange}
+              sx={{ fontFamily: "'Open sans'" }}
+              required
+              error={validacion.correo && !formData.correo}
+            />
+            {validacion.correo && !formData.correo && (
+              <FormHelperText error>Campo obligatorio</FormHelperText>
+            )}
+            <p style={{ margin: 0, color: "black" }}>Contraseña:</p>
+            <TextField
+              hiddenLabel
+              id="outlined-basic"
+              variant="outlined"
+              type="password"
+              name="contrasena"
+              value={formData.contrasena}
+              onChange={handleChange}
+              sx={{ fontFamily: "'Open sans'" }}
+              required
+              error={validacion.contraseña && !formData.contrasena}
+            />
+            {validacion.contraseña && !formData.contrasena && (
+              <FormHelperText error>Campo obligatorio</FormHelperText>
+            )}
+            <Button
+              sx={{
+                color: "white",
+                backgroundColor: "#00338e",
+                fontFamily: "'Open sans'",
+                fontSize: "14px",
+                fontWeight: 600,
+                width: "10rem",
+                marginTop: "1rem",
+                marginBottom: "0.5rem",
+                "&:hover": {
+                  backgroundColor: "#AACBFF",
+                  color: "#00338e",
+                },
+              }}
+              type="submit"
+            >
+              Entrar
+            </Button>
+          </Box>
+        </Box>
       </Box>
     </>
   );
