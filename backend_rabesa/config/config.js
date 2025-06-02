13017cab5a84441bd07cdca80ac0b99bbb308334
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2025-06-02 13:36:45 +0200

    Cambiado variables de la bd

--- backend_rabesa/config/config.js
+++ backend_rabesa/config/config.js
@@ -6,11 +6,11 @@ require("dotenv").config({
 module.exports = {
   port: process.env.PORT || 80,
   db: {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "yamabiko.proxy.rlwy.net",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "test",
+    password: process.env.DB_PASSWORD || "DoCqrfFwsYeXDTLtZpSTHSyjpvgJEtVZ",
     name: process.env.DB_NAME || "rabesafemenino",
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 34243,
   },
   secretKey: process.env.SECRET_KEY || "default_secret",
 };
